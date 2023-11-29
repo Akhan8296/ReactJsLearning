22e81a1b0aeaee0df239314d610893787b9118cd
Akhan8296 <afzzal.bhai@gmail.com>
2023-11-29 23:37:32 +0530

    Add initial files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Random from "./Components/Random";
+function App() {
+  return (
+    <>
+      <h1>Hello Worlds</h1>
+      <h2> Not all</h2>
+      <Random />
+      <Random />
+      <Random />
+      <Random />
+      <Random />
+    </>
+  );
+}
+export default App;
